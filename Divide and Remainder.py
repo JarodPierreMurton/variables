644c7b223b475067b1f1a8d733e347d8b7279105
Jarod Pierre Murton <45271@longroad.ac.uk>
2014-09-12 16:05:45 +0100

    Finished program

---
+++ Divide and Remainder.py
@@ -0,0 +1,76 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+please enter the radius of the circle: 12
+Traceback (most recent call last):
+  File "U:\git\variables\assignment_improvement_exercise.py", line 9, in <module>
+    circumference = 2* math.pi * radius
+TypeError: can't multiply sequence by non-int of type 'float'
+>>> ================================ RESTART ================================
+>>> 
+please enter the radius of the circle: 12
+Traceback (most recent call last):
+  File "U:\git\variables\assignment_improvement_exercise.py", line 9, in <module>
+    circumference = 2* math.pi * radius
+TypeError: can't multiply sequence by non-int of type 'float'
+>>> ================================ RESTART ================================
+>>> 
+please enter the radius of the circle: 
+>>> ================================ RESTART ================================
+>>> 
+please enter the radius of the circle: 12
+The circumference of this circle is 75.4.
+The area of this circle is 452.39.
+>>> ================================ RESTART ================================
+>>> 
+please enter the radius of the circle: 5
+The circumference of this circle is 31.42.
+The area of this circle is 78.54.
+>>> ================================ RESTART ================================
+>>> 
+please enter the radius of the circle: 5
+The circumference of this circle is 31.42.
+The area of this circle is 78.54.
+>>> ================================ RESTART ================================
+>>> 
+please input your first number: 9
+please input your second number: 3
+>>> ================================ RESTART ================================
+>>> 
+please input your first number: 9
+please input your second number: 3
+1.0
+>>> ================================ RESTART ================================
+>>> 
+please input your first number: 9
+please input your second number: 3
+3.0
+>>> ================================ RESTART ================================
+>>> 
+please input your first number: 9
+please input your second number: 3
+3.0
+0.0
+>>> ================================ RESTART ================================
+>>> 
+please input your first number: 9
+please input your second number: 3
+The answer to your first number divided by your second number is 3.0
+0.0
+>>> ================================ RESTART ================================
+>>> 
+please input your first number: 9
+please input your second number: 3
+The answer to your first number divided by your second number is 3.0
+Traceback (most recent call last):
+  File "U:/git/variables/Divide and Remainder.py", line 12, in <module>
+    print("The remainer to your first number divided by your second number is {0}"(remaindersum))
+TypeError: 'str' object is not callable
+>>> ================================ RESTART ================================
+>>> 
+please input your first number: 9
+please input your second number: 3
+The answer to your first number divided by your second number is 3.0
+The remainer to your first number divided by your second number is 0.0
+>>> 
