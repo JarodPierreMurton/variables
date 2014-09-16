9a0fbba976e226dc038b88886adf67f5e941046c
Jarod Pierre Murton <45271@longroad.ac.uk>
2014-09-16 11:13:04 +0100

    Half way through coding will finish easily in next lesson and annotate

---
+++ Swimming pool volume.py
@@ -0,0 +1,8 @@
+#Jarod Pierre Murton
+#Code to work out the volume of a swimming pool
+#16/9/14
+
+length = int(print("Please enter the length of the swimming pool: "))
+width = int(print("Please enter the width of the swimming pool: "))
+shallow_height = int(print("Please enter the depth of the shallow end: "))
+deep_height = int(print("Please enter the depth of the deep end: "))
